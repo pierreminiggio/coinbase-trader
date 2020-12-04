82f0a1290cdd926232200a11d31ae1c36adb19a7
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-12-05 00:59:56 +0100

    HODL

--- src/main.js
+++ src/main.js
@@ -7,22 +7,49 @@ import coinbase from 'coinbase'
 export default function (key, secret) {
     const client = new coinbase.Client({apiKey: key, apiSecret: secret})
 
+    let holdingInWallet
+    client.getAccounts({}, function (err, accounts) {
+        if (err) {
+            throw err
+        }
+        accounts.forEach((acc) => {
+            if (acc.name === 'Portefeuille en BTC') {
+                holdingInWallet = ! (! acc.balance.amount)
+                startTrading()
+            }
+        })
+    })
+
     let isBTCAscending = null
     let lastBTCValue = null
 
-    setInterval(() => {
-        client.getExchangeRates({}, (err, rates) => {
-            if (! err) {
-                const newBTCValue = rates.data.rates.BTC
-                if (lastBTCValue !== null) {
-                    isBTCAscending = isBTCAscending ?
-                        (lastBTCValue <= newBTCValue) :
-                        (lastBTCValue < newBTCValue)
-                    console.log(isBTCAscending ? 'ascending' : 'descending')
+    const startTrading = () => {
+        console.log('Start trading !')
+        return setInterval(() => {
+            client.getExchangeRates({}, (err, rates) => {
+                if (! err) {
+                    const newBTCValue = rates.data.rates.BTC
+                    console.log(newBTCValue)
+                    if (lastBTCValue !== null) {
+                        isBTCAscending = isBTCAscending ?
+                            (lastBTCValue <= newBTCValue) :
+                            (lastBTCValue < newBTCValue)
+                        console.log(isBTCAscending ? 'ascending' : 'descending')
+                        if (holdingInWallet) {
+                            if (isBTCAscending) {
+                                console.log('HODL !')
+                            } else {
+                                if (true /* check if higher than bought price */) {
+                                    console.log('Sell !')
+                                } else {
+                                    console.log('HODL !')
+                                }
+                            }
+                        }
+                    }
+                    lastBTCValue = newBTCValue
                 }
-                lastBTCValue = newBTCValue
-                console.log(newBTCValue)
-            }
-        })
-    }, 31000)
+            })
+        }, 31000)
+    }
 }
